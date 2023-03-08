e3fe948e866795be7cf298da4b50df1efee04bf4
Pranav1326 <visavadiapa@gmail.com>
2023-03-09 01:48:51 +0530

    Money withdrawl from wallet

--- controllers/wallet.js
+++ controllers/wallet.js
@@ -17,6 +17,23 @@ exports.deposit = async (req, res) => {
     }
 }
 
+// Withdrawl
+exports.withdraw = async (req, res) => {
+    const userId = req.body.userId;
+    const withdrawlAmount = req.body.withdraw;
+    try {
+        const updateWallet = await Wallet.updateOne({userId}, {
+            $inc: {money: -withdrawlAmount}
+        }).then(data => {
+            res.status(200).json("Money Withdrawl");
+        }).catch(err => {
+            res.status(500).json(err);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 // Get Wallet Details
 exports.walletDetails = async (req, res) => {
     const userId = req.params.userId;
--- routes/wallet.js
+++ routes/wallet.js
@@ -4,6 +4,9 @@ const walletController = require('../controllers/wallet');
 // Desposit cash
 router.post('/deposit', walletController.deposit);
 
+// Desposit cash
+router.post('/withdraw', walletController.withdraw);
+
 // Get wallet details
 router.get('/:userId', walletController.walletDetails);
 
